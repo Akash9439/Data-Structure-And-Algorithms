8472208056e9cf0696a52688806829a7e7d9797b
Akash Kumar Dash <78552411+Akash9439@users.noreply.github.com>
2021-08-19 21:52:13 +0530

    Initial files are added

--- Array/Reverse a String.py
+++ Array/Reverse a String.py
@@ -1,1 +1,17 @@
+def reverseWord(s):
+    #your code here
+    s=s[::-1]
+    return s
 
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while(t>0):
+        s = input()
+        print(reverseWord(s))
+        t = t-1
+
+# } Driver Code Ends
