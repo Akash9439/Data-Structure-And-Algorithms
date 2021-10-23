68391b14b7ec726f10d23cc3417afefca331d0f6
Akash Kumar Dash <78552411+Akash9439@users.noreply.github.com>
2021-10-23 19:26:09 +0530

    Initial code added

---
+++ Array/Triplet.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<vector<int>> triplet(vector<int>arr,int targetSum)
+{
+    vector<vector<int>> result;
+    int n=arr.size();
+    sort(arr.begin(),arr.end());
+    for(int i=0;i<=n-3;i++)
+    {
+        int j=i+1;
+        int k=n-1;
+        while(j<k)
+        {
+            int current_sum=arr[i];
+            current_sum+=arr[j];
+            current_sum+=arr[k];
+            if(current_sum==targetSum)
+            {
+                result.push_back({arr[i],arr[j],arr[k]});
+                j++;
+                k--;
+            }
+            else if(current_sum>targetSum)
+            {
+                k--;
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> arr{1,2,3,4,5,6,7,8,9,15};
+    int S=18;
+    auto result=triplet(arr,S);
+    for (auto v : result)
+    {
+        for(auto no : v)
+        {
+            cout<<no<<",";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
