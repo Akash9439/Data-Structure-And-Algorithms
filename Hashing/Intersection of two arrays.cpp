6e83934016034958e9f0c8da6b37b8e98c8e69cd
Akash Kumar Dash <78552411+Akash9439@users.noreply.github.com>
2022-07-10 11:57:20 +0530

    Initial code added

---
+++ Hashing/Intersection of two arrays.cpp
@@ -0,0 +1,61 @@
+/*Input:
+n = 5, m = 3
+a[] = {89, 24, 75, 11, 23}
+b[] = {89, 2, 4}
+
+Output: 1
+
+Explanation: 
+89 is the only element 
+in the intersection of two arrays.*/
+// { Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+        unordered_set<int> s;
+        int counter=0;
+        int i;
+        for(i=0;i<n;i++)
+        {
+            s.insert(a[i]);
+        }
+        
+        for(i=0;i<m;i++)
+        {
+            if(s.count(b[i])==1)
+            {
+                counter++;
+                s.erase(b[i]);
+            }
+        }
+        return counter;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a[n], b[m];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        for (int i = 0; i < m; i++) cin >> b[i];
+        Solution ob;
+        cout << ob.NumberofElementsInIntersection(a, b, n, m) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
