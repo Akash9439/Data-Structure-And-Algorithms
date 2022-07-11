45608ad66c705b64a88c37d2dfc5f34f27ade98b
Akash Kumar Dash <78552411+Akash9439@users.noreply.github.com>
2022-07-11 10:51:32 +0530

    Initial code added

---
+++ Search & Sort/Majority Element.cpp
@@ -0,0 +1,68 @@
+/*Input:
+N = 5 
+A[] = {3,1,3,3,2} 
+Output:
+3
+Explanation:
+Since, 3 is present more
+than N/2 times, so it is 
+the majority element.*/
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+      // your code here
+      unordered_map<int,int> m;
+      int i;
+      for(i=0;i<size;i++)
+      {
+          m[a[i]]++;
+      }
+      
+      for(auto x:m)
+      {
+          if(x.second>(size/2))
+          {
+              return x.first;
+          }
+      }
+      return -1;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+
+    int t;
+    cin >> t;
+
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.majorityElement(arr, n) << endl;
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
