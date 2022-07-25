ec4a3046a16e9a2ce7a7950ee739af54ea91918c
Akash Kumar Dash <78552411+Akash9439@users.noreply.github.com>
2022-07-25 10:27:53 +0530

    Initial code added

---
+++ String/Check if strings are rotations of each other or not.cpp
@@ -0,0 +1,61 @@
+/*Given two strings s1 and s2. The task is to check if s2 is a rotated version of the string s1. The characters in the strings are in lowercase.
+
+ 
+
+Example 1:
+
+Input:
+geeksforgeeks
+forgeeksgeeks
+Output: 
+1
+Explanation: s1 is geeksforgeeks, s2 is
+forgeeksgeeks. Clearly, s2 is a rotated
+version of s1 as s2 can be obtained by
+left-rotating s1 by 5 units*/
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution
+{
+    public:
+    //Function to check if two strings are rotations of each other or not.
+    bool areRotations(string s1,string s2)
+    {
+        // Your code here
+        if(s1.length()!=s2.length())
+        {
+            return 0;
+        }
+        s1+=s1;
+        if(s1.find(s2)==-1)
+        {
+            return 0;
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s1;
+        string s2;
+        cin>>s1>>s2;
+        Solution obj;
+        cout<<obj.areRotations(s1,s2)<<endl;
+
+    }
+    return 0;
+}
+
+// } Driver Code Ends
