25bf2778c2d4647af21543a99be9b69acbac597a
Akash Kumar Dash <78552411+Akash9439@users.noreply.github.com>
2022-09-21 16:28:38 +0530

    Create Find all factorial numbers less than or equal to .cpp

---
+++ Mathematics/Find all factorial numbers less than or equal to .cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    vector<long long> factorialNumbers(long long N)
+    {
+        // Write Your Code here
+        vector<long long> v;
+        long long fact=1;
+        long long i=1;
+        while(fact<=N)
+        {
+            fact*=i;
+            i++;
+            if(fact<=N)
+            {
+                v.push_back(fact);
+            }
+        }
+        return v;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long N;
+        cin>>N;
+        Solution ob;
+        vector<long long> ans = ob.factorialNumbers(N);
+        for(auto num : ans){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
+// } Driver Code Ends
