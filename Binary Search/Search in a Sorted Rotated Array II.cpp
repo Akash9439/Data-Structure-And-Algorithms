21a8f1025c8c248626c75e6556653fa52a4a85d0
Akash Kumar Dash <78552411+Akash9439@users.noreply.github.com>
2022-10-11 11:03:51 +0530

    Create Search in a Sorted Rotated Array II.cpp

---
+++ Binary Search/Search in a Sorted Rotated Array II.cpp
@@ -0,0 +1,105 @@
+/*There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+ 
+
+Follow up: This problem is similar to Search in Rotated Sorted Array, but nums may contain duplicates. Would this affect the runtime complexity? How and why?*/
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool Search(int N, vector<int>& A, int Key) 
+    {
+        // Code here
+        int low=0;
+        int high=N-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(Key==A[mid])
+            {
+                return 1;
+            }
+            if(A[low]==A[mid] && A[mid]==A[high])
+            {
+                low+=1;
+                high-=1;
+            }
+            if(A[low]<=A[mid])
+            {
+                if(Key>=A[low] && Key<=A[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else if(A[mid]<=A[high])
+            {
+                if(Key<=A[high] && Key>=A[mid])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        int key;
+        cin >> key;
+
+        Solution obj;
+        cout << obj.Search(n, a, key) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
